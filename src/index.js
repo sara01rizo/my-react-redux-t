c08af35991d17e3d86a359cbbf97b4e4bee03b34
Sara Pulido <sara01rizo@gmail.com>
2022-02-10 16:23:45 +0100

    Add: a Provider from react-redux and props store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./state/store";
 
 ReactDOM.render(
-  
+  <Provider store={store}>
+    {" "}
     <App />
-,
-  document.getElementById('root')
+  </Provider>,
+
+  document.getElementById("root")
 );
-
-
-
